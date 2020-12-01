99fa861c32ef98a1032fad6175cacf820975f692
Gabriel Stölting <gabriel.stoelting@rundequadrate.de>
2020-12-01 17:40:06 +0100

    Improved HDF to PKL conversion

--- HDFtoPKL.py
+++ HDFtoPKL.py
@@ -15,38 +15,87 @@ pkl_filename = asksaveasfilename(filetypes=(("Python Pickle", "*.pkl"), ("All Fi
                                          title="Choose a file.")
 
 hdf = pd.HDFStore(hdf_filename)
-print(hdf.keys())
 
 pkl = Project()
 
 for filename in hdf.conditions.filename.unique():
 
+    hdf_columns = list(hdf.conditions[hdf.conditions.filename == filename].columns.values)
+    print(hdf_columns)
+
+    # We don't need the additional filename column if present
+    if "filename" in hdf_columns:
+        hdf_columns.remove("filename")
+
     # Recording informations
-    dt = float(hdf.conditions[hdf.conditions.filename == filename].dt.values[0])
-    animal = str(hdf.conditions[hdf.conditions.filename == filename].animal.values[0])
-    birthdate = str(hdf.conditions[hdf.conditions.filename == filename].birthdate.values[0])
-    genotype = str(hdf.conditions[hdf.conditions.filename == filename].genotype.values[0])
-    date = str(hdf.conditions[hdf.conditions.filename == filename].date.values[0])
-    sex = str(hdf.conditions[hdf.conditions.filename == filename].sex.values[0])
+    if "dt" in hdf_columns:
+        dt = float(hdf.conditions[hdf.conditions.filename == filename].dt.values[0])
+        hdf_columns.remove("dt")
+    else:
+        dt = 0.1
+        
+    if "animal" in hdf_columns:
+        animal = str(hdf.conditions[hdf.conditions.filename == filename].animal.values[0])
+        hdf_columns.remove("animal")
+    else:
+        animal = "0000"
+        
+    if "birthdate" in hdf_columns:
+        birthdate = str(hdf.conditions[hdf.conditions.filename == filename].birthdate.values[0])
+        hdf_columns.remove("birthdate")
+    else:
+        birthdate = "1900-01-01"
+
+    if "genotype" in hdf_columns:
+        genotype = str(hdf.conditions[hdf.conditions.filename == filename].genotype.values[0])
+        hdf_columns.remove("genotype")
+    else:
+        genotype = "xx"
+        
+    if "date" in hdf_columns:
+        date = str(hdf.conditions[hdf.conditions.filename == filename].date.values[0])
+        hdf_columns.remove("date")
+    else:
+        date = "1900-00-00"
+        
+    if "sex" in hdf_columns:
+        sex = str(hdf.conditions[hdf.conditions.filename == filename].sex.values[0])
+        hdf_columns.remove("sex")
+    else:
+        sex = "unknown"
 
 
     # Conditions
-    begin = hdf.conditions[hdf.conditions.filename == filename].begin.values
-    end = hdf.conditions[hdf.conditions.filename == filename].end.values
-    potassium = hdf.conditions[hdf.conditions.filename == filename].potassium.values
-    angiotensin = hdf.conditions[hdf.conditions.filename == filename].angiotensin.values
-
+    if "begin" in hdf_columns:
+        begin = hdf.conditions[hdf.conditions.filename == filename].begin.values
+        hdf_columns.remove("begin")
+    else:
+        begin = [0]
+    if "end" in hdf_columns:
+        end = hdf.conditions[hdf.conditions.filename == filename].end.values
+        hdf_columns.remove("end")
+    else:
+        end = [1]
+ 
     print(filename, dt, animal, birthdate, genotype, date, sex)
-    print(begin, end, potassium, angiotensin)
+    print(begin, end)
 
     # Create entry for the recording
     information = {"animal": animal, "birthdate": birthdate, "genotype": genotype, "date":date, "sex":sex}
+
+
+
     rec = Recording(file_id=filename, dt=dt, raw_data=hdf["/raw/"+filename], **information)
 
     # Create conditons
 
     for i, start in enumerate(begin):
-        k = {"potassium": float(potassium[i]), "angiotensin": float(angiotensin[i])}
+        k = {}
+        # Add all additional columns from the original:
+        for col in hdf_columns:
+            k[col] = hdf.conditions[hdf.conditions.filename == filename][col].values[i]
+        print(k)
+
         rec.add_condition(int(start), int(end[i]), update_cells=True, **k)
 
 
